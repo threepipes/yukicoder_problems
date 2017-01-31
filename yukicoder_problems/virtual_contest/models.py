dcf4d2d3c6282db15930b7bed83d82385bf6533e
threepipes_s <threepipesatos@gmail.com>
2017-02-01 02:26:40 +0900

    モデルに表示処理を追加，DB上に仮モデルを作成．

--- yukicoder_problems/virtual_contest/models.py
+++ yukicoder_problems/virtual_contest/models.py
@@ -5,11 +5,17 @@ class User(models.Model):
     name = models.CharField(max_length=30)
     user_id = models.IntegerField()
 
+    def __str__(self):
+        return '%s(%d)' % (self.name, self.user_id)
+
 
 class Problem(models.Model):
     name = models.CharField(max_length=50)
     points = models.IntegerField()
     problem_id = models.IntegerField()
+
+    def __str__(self):
+        return '%s[id=%d, %dpt]' % (self.name, self.problem_id, self.points)
 
 
 class Submission(models.Model):
@@ -20,6 +26,11 @@ class Submission(models.Model):
     verdict = models.IntegerField()
     lang = models.CharField(max_length=15)
 
+    def __str__(self):
+        res = '%s: %s' % (str(self.user), str(self.problem))
+        res += ' (%s/%s) %d' % (self.verdict, self.lang, self.submission_id)
+        return res
+
 
 class Contest(models.Model):
     start_time = models.DateTimeField('Contest start time')
@@ -27,3 +38,6 @@ class Contest(models.Model):
     name = models.CharField(max_length=30)
     users = models.ManyToManyField(User)
     problems = models.ManyToManyField(Problem)
+
+    def __str__(self):
+        return self.name
