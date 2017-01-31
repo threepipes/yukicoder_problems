704c7a7c9c3b197a080c344c639a3a0ef20149d9
threepipes_s <threepipesatos@gmail.com>
2017-02-01 02:00:25 +0900

    マイグレーション

--- yukicoder_problems/yukicoder_problems/settings.py
+++ yukicoder_problems/yukicoder_problems/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'virtual_contest.apps.VirtualContestConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
